a81ffb3c20655bae9ef408910fb64a4cfa8b6e45
Victoria Simpri <victoriasimpri@Victorias-MacBook-Pro.local>
2014-12-31 18:58:24 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -82,7 +82,7 @@ static const int GRID_COLUMNS = 10;
     else {
         _totalAlive -= 1;
     }
-    _numVisibleLabel.string = [NSString stringWithFormat:@"%d", _totalAlive];
+    _populationLabel.string = [NSString stringWithFormat:@"%d", _totalAlive];
 }
 
 -(Creature *)creatureForTouchPosition:(CGPoint)touchPosition {
