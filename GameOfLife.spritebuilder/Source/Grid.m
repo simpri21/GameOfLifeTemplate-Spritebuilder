062b279a5f4b441d71f39bb4fc480e21938ea909
Victoria Simpri <victoriasimpri@Victorias-MacBook-Pro.local>
2014-12-31 17:44:57 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -66,7 +66,7 @@ static const int GRID_COLUMNS = 10;
     
 -(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     //get the x,y coordinates of the touch
-    CGPoint touchLocation = [touch locationInView:self.view];
+    CGPoint touchLocation = [touch locationInNode:self];
         
     //get the creature at that location
     Creature *creature = [self creatureForTouchPosition:touchLocation];
